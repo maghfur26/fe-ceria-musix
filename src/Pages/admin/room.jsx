b08a403323b6fb065d7b071cfde006cfa484d4ae
maghfur26 <maghfurhasani9@gmail.com>
2025-05-27 08:59:36 +0700

    change url api for room page

--- src/Pages/admin/room.jsx
+++ src/Pages/admin/room.jsx
@@ -2,14 +2,16 @@ import CardAdmin from "../../Components/elements/CardAdmin";
 import Admin from "./admin";
 
 const RoomPages = () => {
-  return (
-    <Admin>
-      <CardAdmin
-        title={"Daftar Studio"}
-        url={"https://ceriamusicapi-production.up.railway.app/api/room"}
-      />
-    </Admin>
-  );
+    return (
+        <Admin>
+            <CardAdmin
+                title={"Daftar Studio"}
+                url={
+                    "https://serverceriamusic-production.up.railway.app/api/room"
+                }
+            />
+        </Admin>
+    );
 };
 
 export default RoomPages;
